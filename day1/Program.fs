7e77aebf226a203f263f8804e7c7bde8450ef9b9
Johan Hugg <johan.hugg@storytel.com>
2018-12-01 22:51:01 +0100

    Solved day 1 puzzle 1

--- day1/Program.fs
+++ day1/Program.fs
@@ -1,8 +1,10 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open Frequencies
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let result = frequencies.Split("\n") |> Array.toList |> List.map matchStringWithFrequency |> List.choose id |> List.sum
+    printfn "%i" result 
     0 // return an integer exit code
